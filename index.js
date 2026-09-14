f37aae7ec7326a91d2767c85cb69946756848adc
Ashpak009 <m.ashpak009@gmail.com>
2026-09-14 12:24:18 +0530

    docker build

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello Ashpak bro");
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js Docker App!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
